454ed064a15e395cc41f0d71edd866e75946ddec
Thiago Felix <felixthi@gmail.com>
2015-08-06 15:57:00 -0700

    Include ngBind test

--- test/greeting.directive.js
+++ test/greeting.directive.js
@@ -1,5 +1,21 @@
 describe('Greeting Directive', function () {
-  it('prints a message', inject(function () {
-    expect(angular).toBeDefined();
+  it('prints a message', inject(function ($compile, $rootScope) {
+    //the fragment
+    var html = '<div ng-bind="greeting"></div>';
+    //the element
+    var element = angular.element(html);
+    //link function after compile
+    var link = $compile(element);
+
+    //update the scope information
+    $rootScope.greeting = "Hello World!";
+
+    //link the scope in the element
+    link($rootScope);
+
+    //tell the scope to digest the changes
+    $rootScope.$digest();
+
+    expect(element.html()).toEqual('Hello World!');
   }));
 });
